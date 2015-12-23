ab82cc38d2cb95a89709d67d14b0e3eea9f31112
Ian G <yo.ian.g@gmail.com>
2015-12-23 15:27:37 -0500

    Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+//
+//  Package.swift
+//  APIBase
+//
+//  Created by Ian Grossberg on 12/23/15.
+//  Copyright © 2015 Adorkable. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "AdorkableAPIBase",
+    targets: [
+        Target(name: "AdorkableAPIBase")
+    ],
+    exclude: [
+        "APIBaseTests"
+    ]
+)
